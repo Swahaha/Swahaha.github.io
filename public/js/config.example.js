50e7ba791440f0c9f67bf87405bdd6a784be2abb
Swarajh Mehta <swarajhmehta1@gmail.com>
2025-05-17 22:21:22 -0400

    changed path in static.yml

--- public/js/config.example.js
+++
@@ -1,8 +0,0 @@
-// This is a template file. Create a copy named config.js with your actual credentials
-const config = {
-    emailjs: {
-        serviceId: 'YOUR_SERVICE_ID',
-        publicKey: 'YOUR_PUBLIC_KEY',
-        templateId: 'YOUR_TEMPLATE_ID'
-    }
-}; 
